add764bef215a0a7a16277ff5b99e773caf4bfb8
n0npax <marcin@niemira.net>
2017-01-17 23:09:52 +0100

    removed unfunctional scripts with env variables modified Makefile to run celery and app directly

--- setup_env.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export APP_SETTINGS="config.DevelopmentConfig"
-export FLASK_DEBUG=1
-export PY3RUMCAJS_DATABASE_URL='postgresql://rumcajs:turbo@localhost/rumcajs_db'
-# psql -U rumcajs rumcajs_db -h 127.0.0.1
-celery -A app.celery worker  --loglevel=debug 
